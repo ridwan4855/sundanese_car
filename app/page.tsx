b1b37de69d9b4684318cbdca7909bd6f1ecd9a84
Ridwan <maulanar2521@gmail.com>
2024-12-19 22:38:14 +0700

    delete unecessery code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { FooterPart } from "@/components/FooterPart";
 import HeaderPart from "@/components/HeaderPart";
 import { MainPart } from "@/components/MainPart";
-import Image from "next/image";
 
 export default function Home() {
   return (
